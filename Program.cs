cb13fb83625f916e695a1acb6fca44b5f65a45d6
hezuite1112 <30926270+hezuite1112@users.noreply.github.com>
2020-02-05 20:01:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StringMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] tekst = Console.ReadLine().Split(' ');
+                Console.WriteLine(Polocz(tekst[0], tekst[1]));
+            }
+        }
+
+        private static string Polocz(string a, string b)
+        {
+            int dlugosc = a.Length < b.Length ? a.Length : b.Length;
+            string wynik = "";
+            for (int i = 0; i < dlugosc; i++)
+            {
+                wynik += a[i];
+                wynik += b[i];
+            }
+            return wynik;
+        }
+    }
+}
